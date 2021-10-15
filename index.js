acd5226f5cce3847480763208888269e8929c30a
Nick Bechtel <bechtel.nick@gmail.com>
2021-10-15 17:15:59 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+// function superbowlWin(record) {
+//   let yearWin = record.find((element) => element.result === "W");
+// //   if (yearWin === undefined) {
+// //     return yearWin;
+// //   }
 
+//   return yearWin.year;
+// }
+
+// superbowlWin(record);
+
+function superbowlWin(record) {
+  let yearWin = record.find((element) => element.result === "W");
+  return yearWin ? yearWin.year : undefined;
+}
